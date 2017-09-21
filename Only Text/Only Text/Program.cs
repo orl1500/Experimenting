db01ce53a22e7b8f2a82071a8ab3941da1909ef9
Orleans <Orleans@ORLEANS>
2017-09-21 20:27:36 -0300

    First Save

---
+++ Only Text/Only Text/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Only_Text
+{
+
+    class program
+    {
+
+        static void Main ( string [] args )
+        {
+
+
+            Console.WriteLine(" BEM-VINDO ");
+            Console.WriteLine("----------------");
+            Console.WriteLine("TUDO BEM");
+            Console.WriteLine("----------------");
+
+        }
+
+    }
+
+}
